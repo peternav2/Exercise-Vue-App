c8c82af5650b21d97919fc170cbb3a26d77e76ef
Peter Navarrete <peterabcs@gmail.com>
2022-10-26 00:46:27 -0400

    fixed vuerouter issues

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,21 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from './HomeRouter.vue'
+import SignIn from './SignInRouter.vue'
+import YourPosts from './YourPostsRouter.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), //what is this for?
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/signin',
+      name: 'Login',
+      component: SignIn
+    },
+    {
+      path: '/yourposts',
+      name: 'YourPosts',
+      component: YourPosts
     }
   ]
 })
