3ac4026f2ab5aae6faef6e8ae94223309e8d6ac3
Peter Navarrete <peterabcs@gmail.com>
2022-11-05 19:57:46 -0400

    can now refresh routing pages

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const hostname = '127.0.0.1';
 
 app.use('/', express.static('../client/dist'));
 
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: '../client/dist' });
+});
+
 app.listen(port,hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
