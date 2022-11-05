4cb0fa825df1c7393f0a4ddc0f47ee366a769a7c
Peter Navarrete <peterabcs@gmail.com>
2022-11-05 19:46:56 -0400

    created server // cant refresh or fails get

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+const hostname = '127.0.0.1';
+
+app.use('/', express.static('../client/dist'));
+
+app.listen(port,hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
