9bc71346d1b536a124c7f68b2d872a334bf62a37
Peter Navarrete <peterabcs@gmail.com>
2022-11-06 12:59:42 -0500

    init adding products page

---
+++ client/src/router/ProductsRouter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="products">
+        <div class="product" v-for="product in products"> hey</div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { reactive, ref } from 'vue';
+import { RouterLink } from 'vue-router';
+import {getProducts, type Product} from '../stores/products'
+export default {
+    name: 'Products',
+    
+    setup() {
+        const products = reactive(getProducts());
+        return {
+            products
+        }
+    }
+
+}
+</script>
+<style scoped>
+    .products {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .product {
+        width: 30%;
+        margin: 10px;
+        padding: 10px;
+        border: 1px solid grey;
+    }
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './HomeRouter.vue'
 import SignIn from './SignInRouter.vue'
 import YourPosts from './YourPostsRouter.vue'
+import Products from './ProductsRouter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), //what is this for?
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/yourposts',
       name: 'YourPosts',
       component: YourPosts
+    },
+    {
+      path: '/products',
+      name: 'Products',
+      component: Products
     }
   ]
 })
