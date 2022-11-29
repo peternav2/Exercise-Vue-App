a83fc9c827bc578a0fa2c8487e257bbfaa53c090
Peter Navarrete <peterabcs@gmail.com>
2022-11-28 20:28:03 -0500

    workouts controller

---
+++ server/controllers/workouts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const workouts = require('../models/workouts');
+
+const app = express.Router();
+
+
+app
+
+    .get('/', (req,res) => {
+    res.status(200).send(workouts.getWorkouts());
+    })
+    .get('/:id', (req,res) => {
+        const id = req.params.id;
+        const workout = workouts.getWorkout(id);
+        if (workout) {
+            res.status(200).send(workout);
+        } else {
+            res.status(404).send('Workout not found');
+        }
+    });
+
+module.exports = app;
