caa38ac5686b1ce783d9c4131f85fc1e1d616429
Peter Navarrete <peterabcs@gmail.com>
2022-11-28 20:28:16 -0500

    made server able to get workouts

--- server/server.js
+++ server/server.js
@@ -5,10 +5,17 @@ const app = express();
 const port = 3000;
 const hostname = '127.0.0.1';
 
-app.use('/', express.static('../client/dist'));
+app.use('/', express.static('./client/dist'));
+app.use(express.json());
+
+app.get('/error', (req, res) => {
+    res.status(404).send('Error');
+})
+.use('/api/v1/workouts', require('./controllers/workouts'))
+
 
 app.get('*', (req, res) => {
-    res.sendFile('index.html', { root: '../client/dist' });
+    res.sendFile('index.html', { root: './client/dist' });
 });
 
 app.listen(port,hostname, () => {
