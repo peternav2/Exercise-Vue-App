c03273d883f67a94005b509132acabe1a7a8021a
Peter Navarrete <navarrep1@newpaltz.edu>
2022-11-30 13:29:31 -0500

    init creating a submit workout to day page

--- client/src/router/WorkoutsRouter.vue
+++ client/src/router/WorkoutsRouter.vue
@@ -2,13 +2,28 @@
     <div>
         <h1>workout day</h1>
 
-        <button @click.prevent="addWorkoutItem(workouts[i], 4, 12, DayOfWeek.Monday); load()"></button>
+        <button @click.prevent="addWorkoutItem(workouts[i], 4, 12,'monday'); load();"></button>
         <label>press to add</label>
         <button @click="i++"></button>
         <label>press to inc</label>
-        <p>{{( workouts[0])}}</p>
+        <p>{{ DayOfWeek.Monday }}</p>
 
     </div>
+
+
+     <div>
+        <form @submit.prevent="submitItems">
+        <label> Sets:</label>
+        <input type="text" required v-model="sets">
+        <label> Reps:</label>
+        <input type="text" required v-model="reps">
+        <label> Day of the week:</label>
+        <input type="text" required v-model="day">
+        <label> Workout Number:</label>
+        <input type="text" required v-model="workoutNumber">
+    </form>
+    <button @click="submitItems" class="button is-danger"> submit items</button>
+    </div> 
 </template>
 
 <script setup lang="ts">
@@ -18,10 +33,20 @@ import { addWorkoutItem } from '../stores/workoutItem'
 import { DayOfWeek } from '../stores/day';
 import { load } from '../stores/workoutItem'
 
-    let i = 0;
     const workouts = reactive([] as Workout[])
     getWorkouts().then( x => workouts.push(...x.workouts))
+    let i = 0;
 
+
+    const sets = ref("");
+    const reps = ref("");
+    const day = ref("");
+    const workoutNumber = ref("");
+
+    const submitItems = () => {
+      addWorkoutItem(workouts[+workoutNumber.value], +sets.value, +reps.value, day.value);
+      load();
+    };
 
 </script>
 
