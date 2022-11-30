ec5295ccf0b2849d8f1733d6750d3bcf9990254d
Peter Navarrete <peterabcs@gmail.com>
2022-11-29 21:45:48 -0500

    can retrieve all WorkoutItems from all users

--- server/controllers/workoutitems.js
+++ server/controllers/workoutitems.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const workouts = require('../models/workouts')
-const { get, add } = require('../models/workoutitems')
+const { get, add, getAllItems } = require('../models/workoutitems')
 const app = express.Router()
 app
-// .get('/', (req, res) => {
-//     res.send(get(req.params.id))
-// })
+.get('/', (req, res) => {
+    res.send(getAllItems())
+})
 .get('/:id', (req, res) => {
     res.send(get(req.params.id));
 });
