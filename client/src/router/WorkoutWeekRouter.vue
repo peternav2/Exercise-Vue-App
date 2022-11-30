3fe17c036530f9010a5b6110abd262993d520277
Peter Navarrete <peterabcs@gmail.com>
2022-11-30 14:56:14 -0500

    now the workouts show up in the proper day of week

--- client/src/router/WorkoutWeekRouter.vue
+++ client/src/router/WorkoutWeekRouter.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+    import { addWorkoutItem } from '../stores/workoutItem'
+    import type WorkoutItem  from '../stores/workoutItem'
+    import { DayOfWeek } from '../stores/day';
+    import { getWorkouts, getWorkout, type Workout} from '../stores/workouts'
+    import  workoutsItems, { load }  from '../stores/workoutItem'
+    import { computed, reactive } from 'vue';
+    const list = workoutsItems;
+
+    const Monday = computed(() => list.filter(x => x.day === DayOfWeek.Monday));
+    const Tuesday = computed(() => list.filter(x => x.day === DayOfWeek.Tuesday));
+    const Wednesday = computed(() => list.filter(x => x.day === DayOfWeek.Wednesday));
+    const Thursday = computed(() => list.filter(x => x.day === DayOfWeek.Thursday));
+    const Friday = computed(() => list.filter(x => x.day === DayOfWeek.Friday));
+    const Saturday = computed(() => list.filter(x => x.day === DayOfWeek.Saturday));
+    const Sunday = computed(() => list.filter(x => x.day === DayOfWeek.Sunday)); 
+
+
 
 </script>
 
@@ -12,19 +29,31 @@
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Monday</h2>
-                        <p>Monday's workout</p>
+                        <p v-for="item in Monday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Tuesday</h2>
-                        <p>Tuesday's workout</p>
+                        <p v-for="item in Tuesday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Wednesday</h2>
-                        <p>Wednesday's workout</p>
+                        <p v-for="item in Wednesday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -32,19 +61,31 @@
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Thursday</h2>
-                        <p>Thursday's workout</p>
+                        <p v-for="item in Thursday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Friday</h2>
-                        <p>Friday's workout</p>
+                        <p v-for="item in Friday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Saturday</h2>
-                        <p>Saturday's workout</p>
+                        <p v-for="item in Saturday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
             </div>
@@ -52,7 +93,11 @@
                 <div class="column is-one-third">
                     <div class="box">
                         <h2 class="title is-4">Sunday</h2>
-                        <p>Sunday's workout</p>
+                        <p v-for="item in Sunday" :key="item.workout.workoutId">
+                            {{ item.workout.name }}
+                            {{ item.sets }}
+                            {{ item.reps }}
+                        </p>
                     </div>
                 </div>
             </div>
