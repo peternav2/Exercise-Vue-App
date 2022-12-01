fde376b065223888e378a754aa2681c2cdc7db8f
Peter Navarrete <peterabcs@gmail.com>
2022-11-30 20:49:03 -0500

    init mongo

--- server/controllers/workouts.js
+++ server/controllers/workouts.js
@@ -6,17 +6,22 @@ const app = express.Router();
 
 app
 
-    .get('/', (req,res) => {
-    res.status(200).send(workouts.getWorkouts());
+    .get('/', (req,res,next) => {
+        workouts.getWorkouts()
+        .then(x => res.status(200).send(x))
+        .catch(next);
     })
-    .get('/:id', (req,res) => {
+    .get('/:id', (req,res,next) => {
         const id = +req.params.id;
-        const workout = workouts.getWorkout(id);
-        if (workout) {
-            res.status(200).send(workout);
-        } else {
-            res.status(404).send('Workout not found');
-        }
+        workouts.getWorkout(+req.params.id)
+        .then(workout => {
+            if (workout) {
+                res.status(200).send(workout);
+            } else {
+                res.status(404).send('Workout not found');
+            }
+        })
+        .catch(next);
     });
 
 module.exports = app;
--- server/models/workouts.js
+++ server/models/workouts.js
@@ -1,15 +1,26 @@
 const data = require('../data/workouts.json');
+const { connect } = require('./mongo');
 
-
-function getWorkouts() {
-    return data;
+async function collection() {
+    const client = await connect();
+    return client.db('WebHome').collection('workouts');
 }
 
-function getWorkout(id) {
-    return data.workouts.find(w => w.workoutId === id);
+async function getWorkouts() {
+    const db = await collection();
+    const workouts = await db.find().toArray();
+    console.log(workouts);
+    return workouts;
+}
+
+async function getWorkout(workoutId) {
+    const db = await collection();
+    const workout = await db.find({ workoutId: workoutId })
+    return workout;
 }
 
 module.exports = {
+    collection,
     getWorkouts,
     getWorkout
 };
